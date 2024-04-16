ff0b1994f5bef25144376ec8d44d79578d1c1c65
20212TIOT0235 <padilhanna.ls@gmail.com>
2024-04-16 14:40:03 -0300

    começando o código

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -2,15 +2,35 @@ package activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import padilha.anna.lista.R;
 
 public class MainActivity extends AppCompatActivity {
+    static int NEW_ITEM_REQUEST =1;
+    static int PHOTO_PICKER_REQUEST = 1;
+    //Uri photoSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem); //pega o botão
+        fabAddItem.setOnClickListener(new View.OnClickListener() { //pega os clicks
+
+        @Override
+        public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NewItemActivity.class); //intent explícito (navegar para outra página)
+                startActivityForResult(i, NEW_ITEM_REQUEST); //determina que NewItemAcitivity irá retornar dados para a main (NEW_ITEM - id de chamada)
+
+        }
+    });
+
+
+
     }
 }
--- app/src/main/java/activity/NewItemActivity.java
+++ app/src/main/java/activity/NewItemActivity.java
@@ -2,15 +2,32 @@ package activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import padilha.anna.lista.R;
 
 public class NewItemActivity extends AppCompatActivity {
 
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        ImageButton imgCI = findViewById(R.id.imbCI);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT); //intent implícito para abrir galeria
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+        }
+    });
     }
 }
